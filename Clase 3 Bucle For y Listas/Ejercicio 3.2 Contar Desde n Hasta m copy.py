c264f8e9274fe3a96b0250538beedfdc7c8206a6
AndresARL <andres.rojas5722@gmail.com>
2023-07-07 09:25:47 -0400

    Add Excersice 3.3

---
+++ Clase 3 Bucle For y Listas/Ejercicio 3.2 Contar Desde n Hasta m copy.py
@@ -0,0 +1,18 @@
+number = int(input("Introduzca un número: "))
+
+
+prime = True
+dividers = [1]
+for i in range(2, number):
+    if (number % i == 0):
+        dividers.append(i)
+        prime = False
+
+dividers.append(number)
+
+print(f"Los divisores del número son: {dividers}")
+
+if (prime):
+    print("El número es primo")
+else:
+    print("El número no primo")
